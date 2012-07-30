d80c3f864e7224a3bef67dc82243780a4ea26fff
ninj0x <ninj0x@ninj0x-VirtualBox.(none)>
2012-07-30 15:05:55 -0700

    moved all js to single file using Sprockets."

--- source/js/_plugins.js
+++ source/js/_plugins.js
@@ -9,3 +9,25 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
+// Start all the Foundation plugins.
+(function($){
+
+  $(function(){
+    $(document).foundationAlerts();
+    $(document).foundationButtons();
+    $(document).foundationAccordion();
+    $(document).foundationNavigation();
+    $(document).foundationCustomForms();
+    $(document).foundationTabs({callback:$.foundation.customForms.appendCustomMarkup});
+
+    $(document).tooltips();
+    $('input, textarea').placeholder();
+
+    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
+    // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'left'});
+    // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'left'});
+    // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'left'});
+    // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'left'});
+  });
+
+})(jQuery);
